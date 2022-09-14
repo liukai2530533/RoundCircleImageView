f9464baaa6c591284df18d840454e7a21b41437b
liukai <liukai2530533@163.com>
2022-09-14 09:49:43 +0800

    发布版本v101

--- sample/src/main/java/com/newki/sample/DemoActivity.kt
+++ sample/src/main/java/com/newki/sample/DemoActivity.kt
@@ -3,6 +3,7 @@ package com.newki.sample
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import com.newki.circle_round.RoundCircleImageView
 
 class DemoActivity : AppCompatActivity() {
 
@@ -11,6 +12,7 @@ class DemoActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_demo)
 
+        RoundCircleImageView(this)
     }
 
 }
